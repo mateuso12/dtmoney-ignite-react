5ee7326db1f619ef97a5b6c281cb166d18c6fa25
Mateus de Sousa Silva Santos <54694745+mateuso12@users.noreply.github.com>
2021-06-04 11:26:47 -0300

    modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
+import { useState } from "react";
+import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement('#root')
+
 export function App() {
+  const [isNewTransactionOpenModal, setIsNewTransactionOpenModal] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionOpenModal(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionOpenModal(false);
+  }
+
   return (
-    
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+
       <Dashboard />
+
+     <NewTransactionModal 
+     isOpen={isNewTransactionOpenModal}
+     onRequestClose={handleCloseNewTransactionModal}
+     />
+
       <GlobalStyle />
-      
     </>
   );
 }
-
